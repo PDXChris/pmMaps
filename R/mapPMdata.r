9e844bdac0ff96ff98a6020874d4c57dbfe03f25
Chris Prescott <chris.prescott@portlandoregon.gov>
2016-07-07 23:47:05 -0700

    Add warning about streams taking a while to plot

--- R/mapPMdata.r
+++ R/mapPMdata.r
@@ -4,7 +4,8 @@
 #' @param loadObj Path of any data objects that need to be loaded (e.g., wq14)
 #' @param GISdir Path to GIS data
 #' @param ldRivers Should the river shapefile be loaded?
-#' @param ldStreams Should the streams shapefile be loaded?
+#' @param ldStreams Should the streams shapefile be loaded?  Note: this layer
+#' takes considerable time to plot
 #' @import rgdal
 #' @import ggplot2
 #' @import rgeos
@@ -25,6 +26,8 @@ mapPMdata <- function(dfm, vbl, loadObj=NULL, GISdir=NULL, ldStreams=FALSE){
   # streams <- readOGR(GISdir, 'pdx_streams_red.shp')
   # rivers.shp <- readShapePoly(file.path(GISdir, 'pdx_waterbodies.shp'))
   # wtshds.shp <- readShapePoly(file.path(datwd, 'BES_Topo_Watershed.shp'))
+  # freeways <- readOGR(GISdir, 'freeway_new')
+  # arterials <- readOGR(GISdir, 'maj_art')
 
   ### Rivers
   if (ldStreams) load('../pmMapsFiles/data/streamsFonly.rda')
@@ -53,6 +56,12 @@ mapPMdata <- function(dfm, vbl, loadObj=NULL, GISdir=NULL, ldStreams=FALSE){
                  aes(long, lat, group=group),
                  colour='royalblue2', fill='white') +
 
+    # Add highways as landmarks
+#     geom_path(aes(long, lat, group=group), colour='black', lwd=1, data=arter) +
+    # geom_path(aes(long, lat, group=group), colour='red', lwd=1.7, data=freeway) +
+
+
+    # Set map elements
     coord_equal(xlim=c(7600000, 7705000), ylim=c(647000,732000)) +
     xlab('') + ylab('')  + theme_bw() +
 
@@ -63,19 +72,21 @@ mapPMdata <- function(dfm, vbl, loadObj=NULL, GISdir=NULL, ldStreams=FALSE){
           panel.grid.major=element_blank(),
           plot.title=element_text(size=16, face="bold")) +
 
-    # map PAWMAP stations
-    geom_point(data=mapStations@data,
-               aes_string('x', 'y', size=vbl, color='cns')) +
-
-    # Set scales
-    scale_size(name='Mean', range=c(5,12)) +
-    scale_color_manual(name='Detected', values=c('red', 'darkgrey')) +
-    guides(color = guide_legend(override.aes = list(size = 6)))
 
   if (ldStreams) {
     # Map streams
-    p <- p + geom_line(aes(long, lat, group=group), colour='royalblue2', data=streams)
+    p <- p + geom_line(aes(long, lat, group=group),
+                       colour='royalblue2', data=streams)
   }
+
+  # map PAWMAP station data on top
+  p <- p + geom_point(data=mapStations@data,
+             aes_string('x', 'y', size=vbl), colour='darkorange') +
+    # guides(color = guide_legend(override.aes = list(size = 6))) +
+
+    # Set scales
+    # scale_color_manual(name='Detected', values=c('red', 'gray44')) +
+    scale_size(name='Mean', range=c(5,12))
 
   p
 }
