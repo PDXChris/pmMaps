8868bb27b9289ee6fa257932235f6aa53f4fcf8a
Chris Prescott <chris.prescott@portlandoregon.gov>
2016-12-09 15:17:10 -0800

    Change statByStation to make cens variable optional, since it only typically applies to WQ data.

--- R/misc_funs.r
+++ R/misc_funs.r
@@ -3,17 +3,21 @@
 #' @param stat statistical function to calculate.  Mean is the default.
 #' @param station station field name, supplied as a string
 #' @param vbl field name numeric variable to summarize, supplied as a string
+#' @param cens optional field name indicating whether the analyte was detected ('=') or not ('<')
 #' @return a data frame with summary statistic by station
 #' @import plyr
 #' @export
 
 statByStation <- function(dfm, stat = 'mean', station = 'loc_code',
-                          vbl = 'result') {
+                          vbl = 'result', cens = NULL) {
   statByStat_1 <- function(x) {
     stat = do.call(stat, list(x[[vbl]]))
-    cns = ifelse(any(x[['cens']]=='='), 'Detected', 'None\nDetected')
-    pct_det = mean(x$cens == '=')
-    data.frame(stat, cns, pct_det)
+    if (!is.null(cens)) {
+      cns = ifelse(any(x[[cens]]=='='), 'Detected', 'None\nDetected')
+      pct_det = mean(x$cens == '=')
+      data.frame(stat, cns, pct_det)
+    } else {data.frame(stat)}
+
   }
 
   ddply(dfm, station, statByStat_1)
