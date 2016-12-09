9c631b3100e8d765eeffbc4b48780a069f07a6c5
Chris Prescott <chris.prescott@portlandoregon.gov>
2016-12-09 14:47:27 -0800

    fix StatByStation to refer to generic variable instead of 'result'

--- R/misc_funs.r
+++ R/misc_funs.r
@@ -10,7 +10,7 @@
 statByStation <- function(dfm, stat = 'mean', station = 'loc_code',
                           vbl = 'result') {
   statByStat_1 <- function(x) {
-    stat = do.call(stat, list(x[['result']]))
+    stat = do.call(stat, list(x[[vbl]]))
     cns = ifelse(any(x[['cens']]=='='), 'Detected', 'None\nDetected')
     pct_det = mean(x$cens == '=')
     data.frame(stat, cns, pct_det)
