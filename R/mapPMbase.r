76973b924eea38213b791c8346ae87ee36f778f9
Chris Prescott <chris.prescott@portlandoregon.gov>
2017-09-15 14:47:14 -0700

    change filepath for streams data, and make it a default argument that can be changed

--- R/mapPMbase.r
+++ R/mapPMbase.r
@@ -7,10 +7,11 @@
 
 
 mapPMbase <- function(pltArterial=TRUE, ldStreams=FALSE,
-                      xlim=c(7607000, 7700000), ylim=c(650000,729000)){
+                      xlim=c(7607000, 7700000), ylim=c(650000,729000),
+                      streamsLoc='../R/pmMapsFiles/data/streamsFonly.rda'){
 
   ### load streams if TRUE
-  if (ldStreams) load('../pmMapsFiles/data/streamsFonly.rda')
+  if (ldStreams) load(streamsLoc)
 
   p <- ggplot() +
     # Map rivers
