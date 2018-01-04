c2c8a4a8dd95b78e9fbdbf97e6868c0bb6a0374f
Chris Prescott <chris.prescott@portlandoregon.gov>
2018-01-04 15:08:17 -0800

    fix & add mapPMrtg and export to NAMESPACE

--- R/mapPMrtg.r
+++ R/mapPMrtg.r
@@ -1,5 +1,13 @@
-mapPMrtg <- function (dfm, vbl,  legend='', ...)
-{
+#' Map PAWMAP data by size w/ quantile indicated by color
+#' @param dfm Data frame with data to be mapped
+#' @param vbl The field containing the value to map
+#' @param legend Text for the map legend
+#' @import ggplot2
+#' @import classInt
+#' @export
+
+mapPMrtg <- function(dfm, vbl, legend="",  ...){
+
   p <- mapPMbase(...)
   mapStations <- pmStations
   mapStations@data <- merge(mapStations@data, dfm,
@@ -9,14 +17,14 @@ mapPMrtg <- function (dfm, vbl,  legend='', ...)
   class <- with(mapStations@data, classIntervals(dfm[[vbl]], nclr, style="quantile"))
   colcode <- c('forestgreen', 'green', 'yellow', 'darkorange', 'red2')
 
-  mapStations@data$cuts <- cut(mapStations@data[[vbl]], breaks=class$brks, include.lowest=TRUE, right=FALSE)
+  mapStations@data$cuts <- cut(mapStations@data$raneff, breaks=class$brks, include.lowest=TRUE, right=FALSE)
 
   p <-
     p + geom_point(aes(x,y, size=cuts, fill=cuts), shape=21, data=mapStations@data) +
     #   geom_text(aes(x=x, y=y, label=watershed, fontface=2), data=ggwtshd.lab) +
-    scale_size_manual(name=legend, values= c(4,6,8,10, 12),
+    scale_size_manual(name='Urban Metal\nIndex', values= c(4,6,8,10, 12),
                       labels=c('Low', 'Moderately Low', 'Average', 'Moderately High', 'High')) +
-    scale_fill_manual(name=legend, values=colcode, labels=c('Low', 'Moderately Low',
+    scale_fill_manual(name='Urban Metal\nIndex', values=colcode, labels=c('Low', 'Moderately Low',
                                                                          'Average', 'Moderately High', 'High'))
 
   p
