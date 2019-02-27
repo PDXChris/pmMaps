9c9514408699deb3184ad8f6c4e175a4a8bc259f
PDXChris <chris.prescott@portlandoregon.gov>
2019-02-27 14:15:52 -0800

    rivers_pdx - fix bad table ref

--- R/mapPMbase.r
+++ R/mapPMbase.r
@@ -18,10 +18,10 @@ mapPMbase <- function(pltArterial=TRUE, ldStreams=FALSE,
 
   p <- ggplot() +
     # Map rivers
-    geom_polygon(data=rivers[rivers_pdx$hole=='FALSE',],
+    geom_polygon(data=rivers_pdx[rivers_pdx$hole=='FALSE',],
                  aes(long, lat, group=group),
                  colour='royalblue2', fill='royalblue2') +
-    geom_polygon(data=rivers[rivers_pdx$hole==TRUE,],
+    geom_polygon(data=rivers_pdx[rivers_pdx$hole==TRUE,],
                  aes(long, lat, group=group),
                  colour='royalblue2', fill='white') +
 
