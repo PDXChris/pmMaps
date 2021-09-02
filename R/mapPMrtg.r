99cbc14a53f3002a0f6f4fdc76c36fee1cbfbbee
Chris Prescott <chris.prescott@portlandoregon.gov>
2021-09-01 17:40:42 -0700

    Update mapPMrtg.r
    add cutStyle parameter to allow cuts other than quantile

--- R/mapPMrtg.r
+++ R/mapPMrtg.r
@@ -6,30 +6,33 @@
 #' bad (e.g., pollutants = FALSE)?  Default is FALSE.
 #' @param nclr number of colors/breaks in data
 #' @param by.y field in data which matches with station field in shapefile
+#' @param cutStyle method to use in classInterval for developing classes
+#' @param ... additional parameters to pass to mapPMbase
 #' @import ggplot2
 #' @import classInt
 #' @export
 
 mapPMrtg <- function(dfm, vbl, legend="",  highgood=FALSE,
-                     nclr=5, by.y = 'station', ...){
+                     nclr=5, by.y = 'station', cutStyle="quantile", ...){
 
   p <- mapPMbase(...)
   mapStations <- pmStations
   mapStations@data <- merge(mapStations@data, dfm, by.x = "station", by.y = by.y)
 
-  class <- with(mapStations@data, classIntervals(dfm[[vbl]], nclr, style="quantile"))
+  cut_classes <- with(mapStations@data,
+                      classIntervals(dfm[[vbl]], nclr, style=cutStyle))
   colcode <- c('forestgreen', 'green', 'yellow', 'darkorange', 'red2')[1:nclr]
   if (highgood) colcode <- rev(colcode)
 
-  mapStations@data$cuts <- cut(mapStations@data[[vbl]], breaks=class$brks, include.lowest=TRUE, right=FALSE)
+  mapStations@data$cuts <- cut(mapStations@data[[vbl]], breaks=cut_classes$brks,
+                               include.lowest=TRUE, right=FALSE)
+  leg.labels <- c('Low', 'Moderately Low', 'Average', 'Moderately High', 'High')
 
-  p <-
-    p + geom_point(aes(POINT_X, POINT_Y, size=cuts, fill=cuts), shape=21, data=mapStations@data) +
-    #   geom_text(aes(x=x, y=y, label=watershed, fontface=2), data=ggwtshd.lab) +
+  p <- p + geom_point(aes(POINT_X, POINT_Y, size=cuts, fill=cuts),
+                      shape=21, data=mapStations@data) +
     scale_size_manual(name=legend, values= c(4,6,8,10, 12),
-                      labels=c('Low', 'Moderately Low', 'Average', 'Moderately High', 'High')) +
-    scale_fill_manual(name=legend, values=colcode, labels=c('Low', 'Moderately Low',
-                                                                         'Average', 'Moderately High', 'High'))
+                      labels=leg.labels) +
+    scale_fill_manual(name=legend, values=leg.labels)
 
   p
 }
