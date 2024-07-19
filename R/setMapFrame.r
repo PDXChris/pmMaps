16544e00bf3f1e29277838355c92673cf969c592
Chris Prescott <chris.prescott@portlandoregon.gov>
2024-07-18 17:00:20 -0700

    Update setMapFrame.r
    export setMapFrame

--- R/setMapFrame.r
+++ R/setMapFrame.r
@@ -1,5 +1,7 @@
 #' Return Axis Limits for a Map
 #' @param extent The extent of the map frame.
+#' @export
+
 
 setMapFrame <- function(extent = c('Portland', 'West Hills',
                                    'Tryon Creek', 'NW Streams')){
